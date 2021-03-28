246f965cdd7cdcc20fd801aa55d775a801f2224d
wanjiajie <wjj199927@gmail.com>
2021-03-28 18:13:32 +0800

    merge my local branch

--- encyclopedia/urls.py
+++ encyclopedia/urls.py
@@ -1,7 +1,12 @@
 from django.urls import path
 
 from . import views
-
+app_name='encyclopedia'
 urlpatterns = [
-    path("", views.index, name="index")
+    path("index/", views.index, name="index"),
+    path("wiki/<str:title>/",views.renderSubPage, name="subPage"),
+    path("search/",views.search,name="result"),
+    path("add/",views.create,name="create"),
+    path("edit/<str:title>/",views.edit,name="edit"),
+    path("random/",views.randomEntry,name="random")
 ]
